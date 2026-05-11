8d4ccd6cfa1ceae1de7a5d519b7f87341a3d5e53
Nicolas de gheselle <nicolas@degheselle.fr>
2026-05-11 23:16:26 +0200

    Going back to a native Kotlin app

--- front/src/pages/HomePage.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import Container from '@chapelure/common/components/layout/Container.vue';
-</script>
-
-<template>
-    <Container>
-        <span>Home</span>
-    </Container>
-</template>
